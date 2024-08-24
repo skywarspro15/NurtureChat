86565293ad6ef63d0a1eda645b924f1e6f75f268
skwyarspro15 <potatobg648@gmail.com>
2024-08-24 21:04:40 +0800

    Providers + added Gemini

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
+require("dotenv").config();
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const express = require("express");
 const app = express();
-const port = 5501;
+
+const port = process.env.PORT ? process.env.PORT : 5501;
+const server = createServer(app);
+const io = new Server(server);
+
+const ai = require("./providers/gemini");
+
+const fs = require("fs");
+let characters = {};
+
+if (!fs.existsSync("characters.json")) {
+  fs.writeFileSync("characters.json", JSON.stringify(characters));
+} else {
+  characters = JSON.parse(fs.readFileSync("characters.json"));
+}
+
+const provider = new ai.Provider({
+  apiKey: process.env.API_KEY,
+  generationConfig: {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+  },
+  optionalData: {
+    safetySettings: {
+      HARM_CATEGORY_HARASSMENT: "BLOCK_NONE",
+      HARM_CATEGORY_HATE_SPEECH: "BLOCK_NONE",
+      HARM_CATEGORY_DANGEROUS_CONTENT: "BLOCK_NONE",
+      HARM_CATEGORY_SEXUALLY_EXPLICIT: "BLOCK_NONE",
+    },
+  },
+});
 
 app.use(express.static("frontend"));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`NurtureChat instance listening on port ${port}`);
 });
---
+++ providers/gemini.js
@@ -0,0 +1,89 @@
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+class Provider {
+  constructor(data) {
+    this.sessionData = {
+      apiKey: data.apiKey,
+      generationConfig: {
+        temperature: data.generationConfig.temperature,
+        topP: data.generationConfig.topP,
+        topK: data.generationConfig.topK,
+        maxOutputTokens: data.generationConfig.maxOutputTokens,
+        responseMimeType: "text/plain",
+      },
+      optionalData: data.optionalData,
+    };
+    console.log("[PROVIDERS] Gemini provider initialized");
+    return this.sessionData;
+  }
+}
+
+class ChatSession {
+  constructor(data, instructions) {
+    let safetySettings = [];
+    if (data.optionalData.safetySettings) {
+      let safetyData = data.optionalData.safetySettings;
+      Object.keys(safetyData).forEach((category) => {
+        safetySettings.push({
+          category: HarmCategory[category],
+          threshold: HarmBlockThreshold[safetyData[category]],
+        });
+      });
+    }
+    this.genAI = new GoogleGenerativeAI(data.apiKey);
+    this.model = this.genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction: instructions,
+    });
+    this.session = this.model.startChat({
+      generationConfig: data.generationConfig,
+      history: [],
+    });
+  }
+
+  changeConfig(newConfig) {
+    let currentHistory = this.session._history.map((a) => ({ ...a }));
+    this.session = this.model.startChat({
+      generationConfig: newConfig,
+      history: currentHistory,
+    });
+  }
+
+  async send(message) {
+    let result = await this.session.sendMessage(message);
+    return result.response.text();
+  }
+
+  setContext(context) {
+    let geminiFormatted = [];
+    context.forEach((part) => {
+      geminiFormatted.push({
+        role: part.role,
+        parts: [{ text: part.content }],
+      });
+    });
+    this.session._history = geminiFormatted;
+  }
+
+  getContext() {
+    let formatted = [];
+    this.session._history.forEach((part) => {
+      let role = part.role;
+      part.parts.forEach((fragment) => {
+        formatted.push({ role, content: fragment.text });
+      });
+    });
+    return formatted;
+  }
+
+  destroy() {
+    this.genAI = null;
+  }
+}
+
+exports.Provider = Provider;
+exports.ChatSession = ChatSession;
