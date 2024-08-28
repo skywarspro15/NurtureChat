cbd0b0a1e9037576b1d59727552f83f39dc42862
skwyarspro15 <potatobg648@gmail.com>
2024-08-28 11:42:42 +0800

    Message menu triggers on press instead of long press

--- frontend/menus/chat.js
+++ frontend/menus/chat.js
@@ -24,56 +24,6 @@ const menu = {
       overflow: "auto",
     });
 
-    function bindPressHold(item, cb) {
-      let timerID;
-      let counter = 0;
-
-      let pressHoldEvent = new CustomEvent("pressHold");
-      let pressHoldDuration = 50;
-
-      item.addEventListener("mousedown", pressingDown, false);
-      item.addEventListener("mouseup", notPressingDown, false);
-      item.addEventListener("mouseleave", notPressingDown, false);
-
-      item.addEventListener("touchstart", pressingDown, false);
-      item.addEventListener("touchend", notPressingDown, false);
-
-      // Listening for our custom pressHold event
-      item.addEventListener("pressHold", cb, false);
-
-      function pressingDown(e) {
-        // Start the timer
-        requestAnimationFrame(timer);
-
-        e.preventDefault();
-
-        console.log("Pressing!");
-      }
-
-      function notPressingDown(e) {
-        // Stop the timer
-        cancelAnimationFrame(timerID);
-        counter = 0;
-
-        console.log("Not pressing!");
-      }
-
-      //
-      // Runs at 60fps when you are pressing down
-      //
-      function timer() {
-        console.log("Timer tick!");
-
-        if (counter < pressHoldDuration) {
-          timerID = requestAnimationFrame(timer);
-          counter++;
-        } else {
-          console.log("Press threshold reached!");
-          item.dispatchEvent(pressHoldEvent);
-        }
-      }
-    }
-
     function createDialog(title) {
       let dialog = new Html("md-dialog")
         .attr({ open: true })
@@ -132,7 +82,7 @@ const menu = {
       new Html("p").text(text).appendTo(bubble).styleJs({
         color: "var(--md-sys-color-inverse-surface)",
       });
-      bindPressHold(bubble.elm, cb);
+      bubble.on("click", cb);
       new Html("br").appendTo(chat);
       chat.elm.scrollTop = chat.elm.scrollHeight;
     }
@@ -140,7 +90,6 @@ const menu = {
     conversationIndex = args.messages.length;
     let msgMenu = (message) => {
       console.log(message);
-      navigator.vibrate(50);
       let actionsList = [
         {
           icon: "content_copy",
@@ -165,6 +114,9 @@ const menu = {
           },
         },
       ];
+      setTimeout(() => {
+        navigator.vibrate(50);
+      }, 50);
       let dialog = createDialog("Message actions");
       dialog.actions.clear();
       let list = new Html("md-list").appendTo(dialog.contents).styleJs({
